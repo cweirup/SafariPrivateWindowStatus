8f318a4ff0d3ebc802922096cdc7532a1d402b3d
Chris Weirup <cweirup@gmail.com>
2020-05-05 21:11:07 -0400

    Updated toolbar and app icons.

--- Private Window Status Extension/SafariExtensionHandler.swift
+++ Private Window Status Extension/SafariExtensionHandler.swift
@@ -13,7 +13,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
         page.getPropertiesWithCompletionHandler { properties in
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+//            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
         }
     }
     
@@ -25,23 +25,39 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
         // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
         //validationHandler(true, "")
+        NSLog("Entered validateToolbarItem")
         
         window.getActiveTab{ (activeTab) in
             activeTab?.getActivePage{ (activePage) in
                 activePage?.getPropertiesWithCompletionHandler{ (pageProperties) in
-//                    var image: NSImage
-                    if (pageProperties!.usesPrivateBrowsing) {
-                        //image = NSImage(named: "privateicon")!
-                        validationHandler(true, "")
-                    }
-                    else {
-                        //image = NSImage(named: "nonprivateicon")!
+                    var image: NSImage
+//                    NSLog("validateToolbarItem Private?: \(pageProperties!.usesPrivateBrowsing)")
+                    guard let isPrivate = pageProperties?.usesPrivateBrowsing else {
                         validationHandler(false, "")
+                        return
                     }
                     
-//                    window.getToolbarItem { (toolbarItem) in
-//                        toolbarItem?.setImage(image)
+                    switch isPrivate {
+                    case true:
+                        image = NSImage(named: "privateicon")!
+                        validationHandler(true, "")
+                    case false:
+                        image = NSImage(named: "nonprivateicon")!
+                        validationHandler(false, "")
+                    }
+
+//                    if (pageProperties!.usesPrivateBrowsing) {
+//                        //image = NSImage(named: "privateicon")!
+//                        validationHandler(true, "")
 //                    }
+//                    else {
+//                        //image = NSImage(named: "nonprivateicon")!
+//                        validationHandler(false, "")
+//                    }
+                    
+                    window.getToolbarItem { (toolbarItem) in
+                        toolbarItem?.setImage(image)
+                    }
                 }
             }
         }
